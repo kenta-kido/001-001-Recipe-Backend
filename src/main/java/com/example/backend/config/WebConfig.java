88d3aa5b9133f4b0982629cf8f478596a262fe89
Kenta Kido <kido.0117.1895@gmail.com>
2024-11-20 16:29:38 +0100

    webconfigのエラーを修正

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -1,16 +1,10 @@
-package com.example.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // 全てのリクエストを index.html にフォワード
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/index.html");
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}/**{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/index.html");
+        // すべてのリクエストを index.html にフォワード
+        registry.addViewController("/{spring}").setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring}").setViewName("forward:/index.html");
+        registry.addViewController("/{spring}/**").setViewName("forward:/index.html");
     }
 }
