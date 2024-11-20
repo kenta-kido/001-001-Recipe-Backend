545c119656a5d17c7c1b5ade7be7b689a729878c
Kenta Kido <kido.0117.1895@gmail.com>
2024-11-20 16:30:23 +0100

    webcongfigの構文ミス修正

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -1,3 +1,9 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
